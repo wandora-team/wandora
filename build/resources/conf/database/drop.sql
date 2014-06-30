be67b3e8ee5e0f827c4aa0fe83471de3098d361e
akivela <akivela@gripstudios.com>
2014-06-30 21:45:30 +0300

    deleted build and dist folders.

--- build/resources/conf/database/drop.sql
+++
@@ -1,9 +0,0 @@
-
-drop table MEMBER;
-drop table ASSOCIATION;
-drop table SUBJECTIDENTIFIER;
-drop table VARIANTSCOPE;
-drop table VARIANT;
-drop table DATA;
-drop table TOPICTYPE;
-drop table TOPIC;
