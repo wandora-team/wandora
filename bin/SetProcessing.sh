f33eb751cf3ce296d903a5134d9e0a4a333f5bd1
akivela <akivela@gripstudios.com>
2015-06-12 23:07:56 +0300

    Updating Processing to 2.2. Hiding some deprecated extractors.

--- bin/SetProcessing.sh
+++ bin/SetProcessing.sh
@@ -5,7 +5,7 @@
 # Depending on JRE version you may have to change
 # the P_ARCH value to 32 or 64.
 P_ARCH=32
-JRI_LD_PATH=lib/processing/serial/linux$P_ARCH:lib/processing/opengl/linux$P_ARCH:
+# JRI_LD_PATH=lib/processing/serial/linux$P_ARCH:lib/processing/opengl/linux$P_ARCH:
 if test -z "$LD_LIBRARY_PATH"; then
   LD_LIBRARY_PATH=$JRI_LD_PATH
 else
@@ -13,6 +13,6 @@ else
 fi
 export LD_LIBRARY_PATH
 WANDORACLASSES=$WANDORACLASSES:lib/processing/*
-WANDORACLASSES=$WANDORACLASSES:lib/processing/serial/*
-WANDORACLASSES=$WANDORACLASSES:lib/processing/opengl/*
-WANDORALIB=$WANDORALIB:lib/processing/serial/linux$P_ARCH:lib/processing/opengl/linux$P_ARCH
+# WANDORACLASSES=$WANDORACLASSES:lib/processing/serial/*
+# WANDORACLASSES=$WANDORACLASSES:lib/processing/opengl/*
+# WANDORALIB=$WANDORALIB:lib/processing/serial/linux$P_ARCH:lib/processing/opengl/linux$P_ARCH
