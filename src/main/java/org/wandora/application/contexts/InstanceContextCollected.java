1080054cf3e72a839ce4992cb3ff4b50b0672efc
Aki Kivela <akivela@gripstudios.com>
2026-07-20 22:12:33 +0300

    Fixes.

--- src/main/java/org/wandora/application/contexts/InstanceContextCollected.java
+++ src/main/java/org/wandora/application/contexts/InstanceContextCollected.java
@@ -54,7 +54,7 @@ public class InstanceContextCollected extends LayeredTopicContext {
     
     
     @Override
-    public Iterator<Topic> getContextObjects() {
+    public Iterator<?> getContextObjects() {
         return collectInstancesOf( super.getContextObjects() );
     }
     
@@ -62,7 +62,7 @@ public class InstanceContextCollected extends LayeredTopicContext {
     
     
     
-    public Iterator<Topic> collectInstancesOf(Iterator<Topic> topics) {
+    public Iterator<?> collectInstancesOf(Iterator<?> topics) {
         if(topics == null) return null;
         List<Topic> contextTopics = new ArrayList<>();
         Collection<Topic> instanceTopics = null;
